f2d3cd22836992059e52ddd8fa09299de2adc1f7
Hamza Saleem <Hamza Saleem@WE4DO-PC>
2020-05-15 23:12:13 +0500

    Changes

--- TeamTracker2.0/TeamTracker2.0/UC_SessionLogs.cs
+++ TeamTracker2.0/TeamTracker2.0/UC_SessionLogs.cs
@@ -7,14 +7,154 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GridViewExample;
 
 namespace TeamTracker2._0
 {
     public partial class UC_SessionLogs : UserControl
     {
-        public UC_SessionLogs()
+        GridViewHelper gridViewHelper = null;
+        Form prevForm = null;
+        
+        public UC_SessionLogs(Form form )
         {
+            this.prevForm = form;
             InitializeComponent();
+        }
+
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UC_SessionLogs_Load(object sender, EventArgs e)
+        {
+            generateGridView();
+        }
+        private void generateGridView()
+        {
+            // Implementation 
+
+            gridViewHelper = new GridViewHelper(" taskID AS TaskID, TaskDesc , TaskTitle AS TaskName,TaskStatus AS Status,LastDate AS LastDate", "task", null, prevForm, bunifuCustomDataGrid1);
+
+            //gridViewHelper.addProgressBarColumn("Progress", gridViewHelper.getColumnValues("Progress"), Color.LightGreen, 5);
+
+            //gridViewHelper.addEditColumn();
+            //gridViewHelper.addDeleteColumn();
+
+            //gridViewHelper.hideColumn(0);
+            //gridViewHelper.hideColumn(1);
+            //gridViewHelper.hideColumn(2);
+
+            //gridViewHelper.enableSearchEngine(new String[] { "Status", "TaskName" });
+
+      //      gridViewHelper.addColumnWidth(3, 168);
+         //   gridViewHelper.addColumnWidth(4, 100);
+        //    gridViewHelper.addColumnWidth(5, 81);
+       //     gridViewHelper.addColumnWidth(6, 50);
+        //    gridViewHelper.addColumnWidth(7, 53);
+       //     gridViewHelper.addColumnWidth(8, 53);
+     //       gridViewHelper.adjustWidth();
+
+           // gridViewHelper.addContentClickEvent(bunifuCustomDataGrid_CellContentClick);
+
+        }
+//        private void bunifuCustomDataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+//        {
+//            GlassyPanel panel = new GlassyPanel();
+
+//            if (gridViewHelper.IsEditEnable && e.ColumnIndex == bunifuCustomDataGrid1.ColumnCount - 2)
+//            {
+//                string value = this.bunifuCustomDataGrid1.Rows[e.RowIndex].Cells[1].FormattedValue.ToString();
+//                //        MessageBox.Show(value);
+//                panel.Height = this.Height;
+//                panel.Width = this.Width;
+//                this.Controls.Add(panel);
+//                panel.BringToFront();
+//                DataManupilation dm = new DataManupilation(panel);
+
+//                //   var c = GetAll(dm, typeof(TextBox));
+
+//                DataTable reader = ManData.getDataReader(" * ", "task", " taskID=" + value);
+//                foreach (DataRow row in reader.Rows)
+//                {
+//                    dm.Taskid.Text = row["TaskId"].ToString();
+//                    dm.TaskTitle1.Text = row["TaskTitle"].ToString();
+//                    dm.TaskDesc1.Text = row["TaskDesc"].ToString();
+//                    dm.TxtProgress.Text = row["Progress"].ToString();
+//                    dm.TaskStatus1.Text = row["TaskStatus"].ToString();
+//                    dm.TaskStatus1.Text = row["AssignedTo"].ToString();
+//                    dm.LastDate1.Text = row["LastDate"].ToString();
+//                    dm.Extension_reason1.Text = row["Extension_reason"].ToString();
+
+//                }
+
+//                /*
+                 
+//SELECT TaskStatus FROM task GROUP BY TaskStatus
+
+//SELECT UserID,NAME FROM USER GROUP BY UserID
+//                 */
+//                DataTable dt_Task = ManData.getDataReader("TaskStatus", "task", "1=1  GROUP BY TaskStatus");
+
+//                //foreach (DataRow row in dt_Task.Rows)
+//                //{
+//                //    ComboboxItem item = new ComboboxItem();
+//                //    item.Text = "Item text1";
+//                //    item.Value = 12;
+
+//                //    dm.TaskStatus1.Items.ad
+
+//                //    comboBox1.SelectedIndex = 0;
+
+//                //    MessageBox.Show((comboBox1.SelectedItem as ComboboxItem).Value.ToString());
+//                //    dm.TaskStatus1.Text = row["TaskStatus"].ToString();
+
+//                //}
+
+//                DataTable dt_User = ManData.getDataReader("UserID,Name", "USER", "1=1  GROUP BY UserID");
+
+//                // TaskId,TaskTitle,TaskDesc,Progress,TaskStatus,AssignedTo,LastDate,DateOfCreation,LastUpdated,Extension_reason
+//                try
+//                {
+//                    //using (var conn = ManData.getConnection())
+//                    //{
+//                    //    conn.Open();
+//                    //    while (reader.Read())
+//                    //    {
+//                    //        dm.Taskid.Text = reader["TaskId"].ToString();
+//                    //        dm.TaskTitle1.Text = reader["TaskTitle"].ToString();
+//                    //        dm.TaskDesc1.Text = reader["TaskDesc"].ToString();
+//                    //        dm.TxtProgress.Text = reader["Progress"].ToString();
+//                    //        dm.TaskStatus1.Text = reader["TaskStatus"].ToString();
+//                    //        dm.TaskStatus1.Text = reader["AssignedTo"].ToString();
+//                    //        dm.LastDate1.Text = reader["LastDate"].ToString();
+//                    //        dm.Extension_reason1.Text = reader["Extension_reason"].ToString();
+
+//                    //    }
+//                    //}
+
+//                }
+//                catch (Exception ex)
+//                {
+//                    MessageBox.Show(ex.Message);
+//                }
+//                dm.Show();
+
+
+//            }
+//            else if (gridViewHelper.IsDeleteEnable && e.ColumnIndex == bunifuCustomDataGrid1.ColumnCount - 1)
+//            {
+//                string value =
+//                this.bunifuCustomDataGrid1.Rows[e.RowIndex].Cells[1].FormattedValue.ToString();
+//                //          MessageBox.Show(value);
+//            }
+//        }
+
+        private void bunifuCustomDataGrid1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
--- TeamTracker2.0/TeamTracker2.0/UC_UserStatistics.Designer.cs
+++ TeamTracker2.0/TeamTracker2.0/UC_UserStatistics.Designer.cs
@@ -28,33 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.xuiFlatTab1 = new XanderUI.XUIFlatTab();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.xuiFlatTab1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // xuiFlatTab1
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(17, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "User Statistics";
+            this.xuiFlatTab1.ActiveHeaderColor = System.Drawing.Color.DodgerBlue;
+            this.xuiFlatTab1.ActiveTextColor = System.Drawing.Color.White;
+            this.xuiFlatTab1.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.xuiFlatTab1.Controls.Add(this.tabPage2);
+            this.xuiFlatTab1.Controls.Add(this.tabPage1);
+            this.xuiFlatTab1.HeaderBackgroundColor = System.Drawing.Color.White;
+            this.xuiFlatTab1.InActiveHeaderColor = System.Drawing.Color.RoyalBlue;
+            this.xuiFlatTab1.InActiveTextColor = System.Drawing.Color.White;
+            this.xuiFlatTab1.ItemSize = new System.Drawing.Size(240, 16);
+            this.xuiFlatTab1.Location = new System.Drawing.Point(12, 12);
+            this.xuiFlatTab1.Name = "xuiFlatTab1";
+            this.xuiFlatTab1.PageColor = System.Drawing.Color.White;
+            this.xuiFlatTab1.SelectedIndex = 0;
+            this.xuiFlatTab1.ShowBorder = true;
+            this.xuiFlatTab1.Size = new System.Drawing.Size(567, 428);
+            this.xuiFlatTab1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 20);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(559, 404);
+            this.tabPage2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.metroLabel1);
+            this.panel1.Location = new System.Drawing.Point(-4, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(567, 404);
+            this.panel1.TabIndex = 3;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel1.ForeColor = System.Drawing.Color.White;
+            this.metroLabel1.Location = new System.Drawing.Point(4, 4);
+            this.metroLabel1.Margin = new System.Windows.Forms.Padding(10, 10, 3, 0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(560, 34);
+            this.metroLabel1.Style = MetroFramework.MetroColorStyle.White;
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "LEADERBOARD";
+            this.metroLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.metroLabel1.UseCustomBackColor = true;
+            this.metroLabel1.UseCustomForeColor = true;
+            this.metroLabel1.UseStyleColors = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Location = new System.Drawing.Point(4, 20);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(559, 404);
+            this.tabPage1.TabIndex = 2;
             // 
             // UC_UserStatistics
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.xuiFlatTab1);
             this.Name = "UC_UserStatistics";
+            this.Size = new System.Drawing.Size(590, 454);
+            this.xuiFlatTab1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private XanderUI.XUIFlatTab xuiFlatTab1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.TabPage tabPage1;
     }
 }
